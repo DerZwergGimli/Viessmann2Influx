2bec232bb0b5fe01b17b41756f5c775b78733f1f
DerZwergGimli <paule22@protonmail.com>
2021-11-08 14:37:49 +0100

    path fix

--- docker/runViessmann.sh
+++ docker/runViessmann.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Staring fetching Viessmann API..." >> /proc/1/fd/1
-cd /home/python/Viessmann_API_InfluxDB_Logger && python3 python_viessmannapi.py >> /dev/null
+cd /home/python/Viessmann2Influx && python3 v2i_run.py >> /proc/1/fd/1
 echo "...Fetched Viessmann API!" >> /proc/1/fd/1
